cf4b85e466cf576839c93913ec0b1e21c9f0a9bd
Jonathan Roberts <jonrob68@gmail.com>
2020-02-22 11:48:38 -0600

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+function getDogImage(inputValue) {
+    let url = `https://dog.ceo/api/breed/${inputValue}/images/random`;
+     fetch(url)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+  }
+  
+  function displayResults(responseJson) {
+    console.log(responseJson);
+    if (responseJson.status == 'error' && responseJson.code == '404'){
+     $('.results-img').html(`<p> Error code :'404', message :'Breed not found'</p>`);
+    } else {
+        $('.results-img').html(
+      `<img src="${responseJson.message}">`);
+    }
+    
+  }
+
+function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      let inputValue = $('#breed-name').val();
+      getDogImage(inputValue);   
+    });
+  }
+  
+  $(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+  });
